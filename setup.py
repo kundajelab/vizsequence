e1b783b1373d81294bd79d55e16a1c537848616c
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2019-01-25 10:46:07 -0800

    put the viz sequence code in

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+if __name__== '__main__':
+    setup(include_package_data=True,
+          description='Utilities for visualizing dna sequences',
+          long_description="""Utilities for visualizing DNA sequences""",
+          url='https://github.com/kundajelab/vizsequence',
+          version='0.1.0.0',
+          packages=find_packages(),
+          setup_requires=[],
+          install_requires=['numpy>=1.9', 'matplotlib>=2.2.2'],
+          scripts=[],
+          name='vizsequence')
---
+++ vizsequence/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import division, print_function, absolute_import
+from . import viz_sequence
+from viz_sequence import plot_weights, ic_scale
